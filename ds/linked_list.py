e75a1bd089bb17d785d9726ceffd9386ab4eb20a
Raymond Tukpe <rtukpe@gmail.com>
2020-08-14 10:02:53 +0100

    initial commit

---
+++ ds/linked_list.py
@@ -0,0 +1,21 @@
+class LinkedList:
+    def __init__(self):
+        self.head = null
+
+    def get_head(self):
+        return self.head
+
+    def add(self, value):
+        pass
+
+    def remove(self, head, value):
+        pass
+
+    def prepend(self, value):
+        pass
+
+    def walk(self, head):
+        pass
+
+    def walkReverse(self, head, tail):
+        pass
